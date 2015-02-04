7947922e167ef8a11fccdad0f7dc79ef502884b0
kheradmand <a.i.kheradmand@gmail.com>
2015-02-03 18:44:58 -0600

    get input file

--- src/main/java/replaymop/Main.java
+++ src/main/java/replaymop/Main.java
@@ -1,7 +1,29 @@
 package replaymop;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+
 public class Main {
+	public static Parameters parameters;
+	
+	private static void parserArguments(String[] args){
+		parameters = new Parameters();
+		JCommander parameterParser;
+		try{
+			parameterParser = new JCommander(parameters, args);
+		}catch (ParameterException pe){
+			System.out.println(pe.getMessage());
+			return;
+		}
+		parameterParser.setProgramName("replaymop");
+		if (parameters.inputFile == null){
+			parameterParser.usage();
+			System.exit(1);
+		}
+	}
 	public static void main(String[] args){
+		parserArguments(args);
+		
 		
 	}
 }
---
+++ src/main/java/replaymop/Parameters.java
@@ -0,0 +1,9 @@
+package replaymop;
+
+import com.beust.jcommander.*;
+
+public class Parameters {
+	@Parameter(description = "Input file")
+	public String inputFile;
+
+}
