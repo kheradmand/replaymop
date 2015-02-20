e7683769a284bd64e2766974022f5759855f61f4
Ali Kheradmand <a.i.kheradmand@gmail.com>
2015-02-20 15:26:32 -0600

    began to automate RVPredictOld tests

---
+++ src/test/java/replaymop/RVPredictOldTest.java
@@ -0,0 +1,20 @@
+package replaymop;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class RVPredictOldTest {
+	
+	
+	@Parameters(name = "{0}")
+	public static Collection<Object[]> data() {
+        ArrayList<Object[]> data = new ArrayList<Object[]>();
+        
+        return data;
+	};
+}
