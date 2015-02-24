03acdb70c4207d2105c6e3f57f4a3de50568222f
kheradmand <a.i.kheradmand@gmail.com>
2015-02-23 21:09:29 -0600

    start to read rv predict's log events

--- src/main/java/replaymop/parser/RVPredictLogParser.java
+++ src/main/java/replaymop/parser/RVPredictLogParser.java
@@ -2,17 +2,48 @@ package replaymop.parser;
 
 import java.io.File;
 
+import com.runtimeverification.rvpredict.config.Configuration;
+import com.runtimeverification.rvpredict.log.EventItem;
+import com.runtimeverification.rvpredict.log.OfflineLoggingFactory;
+import com.runtimeverification.rvpredict.trace.Event;
+import com.runtimeverification.rvpredict.trace.EventUtils;
+import com.runtimeverification.rvpredict.trace.TraceCache;
+
 import replaymop.ReplayMOPException;
-import replaymop.replayspecification.ReplaySpecification;
 
 public class RVPredictLogParser extends Parser{
 	
+	OfflineLoggingFactory metaData;
+	TraceCache trace;
+	
 	public RVPredictLogParser(File logFolder) {
+		Configuration config = new Configuration();
+		config.outdir = logFolder.toString();
+		metaData = new OfflineLoggingFactory(config);
+		trace = new TraceCache(metaData);
+		initSpec();
+	}
+	
+	private void initSpec(){
+		//TODO: sharedvars, threads
+		spec.addAfterSyncDefault();
+		spec.addBeforeSyncDefault();
 		
 	}
 	
 	@Override
 	protected void startParsing() throws ReplayMOPException{
+		try{
+			EventItem eventItem;
+			for (int index = 0; ((eventItem = trace.getEvent(index)) != null); index++){
+				Event event = EventUtils.of(eventItem);
+				event.getType();
+				//TODO: schedule, thread creation order
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			throw new ReplayMOPException("error in parsing log file");
+		}
 		
 	}
 	
