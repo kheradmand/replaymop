350a466b484d04acb4f5dc9af84d00a1199126c6
kheradmand <a.i.kheradmand@gmail.com>
2015-02-24 17:00:15 -0600

    checkpoint

--- src/main/java/replaymop/parser/RVPredictLogParser.java
+++ src/main/java/replaymop/parser/RVPredictLogParser.java
@@ -10,6 +10,7 @@ import com.runtimeverification.rvpredict.trace.EventUtils;
 import com.runtimeverification.rvpredict.trace.TraceCache;
 
 import replaymop.ReplayMOPException;
+import replaymop.replayspecification.ReplaySpecification;
 
 public class RVPredictLogParser extends Parser{
 	
@@ -22,10 +23,15 @@ public class RVPredictLogParser extends Parser{
 		metaData = new OfflineLoggingFactory(config);
 		trace = new TraceCache(metaData);
 		initSpec();
+		
+		for (int i=1;metaData.getVarSig(i)!=null;i++)
+			System.out.println(String.format("%d: %s", i, metaData.getVarSig(i)));
+		System.out.println("--");
 	}
 	
 	private void initSpec(){
 		//TODO: sharedvars, threads
+		this.spec = new ReplaySpecification();
 		spec.addAfterSyncDefault();
 		spec.addBeforeSyncDefault();
 		
@@ -35,16 +41,25 @@ public class RVPredictLogParser extends Parser{
 	protected void startParsing() throws ReplayMOPException{
 		try{
 			EventItem eventItem;
-			for (int index = 0; ((eventItem = trace.getEvent(index)) != null); index++){
+			for (int index = 1; ((eventItem = trace.getEvent(index)) != null); index++){
 				Event event = EventUtils.of(eventItem);
 				event.getType();
+				spec.threads.add(event.getTID());
+				System.out.println(event);
 				//TODO: schedule, thread creation order
 			}
 		}catch(Exception e){
 			e.printStackTrace();
 			throw new ReplayMOPException("error in parsing log file");
 		}
+		System.out.println("---");
+		System.out.println(spec);
 		
+	}
+	
+	static public void main(String[] args){
+		Parser parser = new RVPredictLogParser(new File(args[0]));
+		parser.parse();
 	}
 	
 
