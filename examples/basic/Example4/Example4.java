506c1703581d1a185bcf136222448ce1872cb8d9
kheradmand <a.i.kheradmand@gmail.com>
2015-03-27 14:26:14 -0500

    added Example4

---
+++ examples/basic/Example4/Example4.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*******************************************************************************
+ * Copyright (c) 2015 University of Illinois
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+/**
+ * 
+ * @author ali kheradmand
+ *
+ */
+public class Example4 {
+	List<Something> sa;
+	
+	static class Something {
+		int x;
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		(new Example4()).start();
+	}
+	
+	void start(){
+		System.out.println("main start");
+
+		sa = new ArrayList<Something>();
+		System.out.println("main after list creation");
+
+		sa.add(new Something());
+		try
+		{
+			
+			System.out.println("main before thread creation");
+			MyThread t1 = new MyThread();
+			MyThread t2 = new MyThread();
+			
+			
+			System.out.println("main before thread start");
+			t1.start();
+			t2.start();
+			
+			System.out.println("main before thread join");
+			t1.join();
+			t2.join();
+			
+				
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	 class MyThread extends Thread
+	{
+		
+		public void run()
+		{
+			System.out.println("MyThread run");
+
+			sa.add(new Something());
+			System.out.println("MyThread after adding element");
+			sa.get(0).x = 12;
+			System.out.println("MyThread after setting x = 12");
+			System.out.println(sa.get(1));
+			List<Something> kk = sa;
+			kk.add(new Something());
+		}
+	}
+}
